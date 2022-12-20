132765a71fa99faff0a25ace4d63e358e055c05a
Eduardo Sánchez <edu.satoe@gmail.com>
2022-12-20 23:20:41 +0100

    Add files via upload

---
+++ digispark/Talker.ino
@@ -0,0 +1,36 @@
+#define LAYOUT_SPANISH
+#include "DigiKeyboard.h"
+void setup() {
+}
+
+void loop() {
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(100);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(100);
+  DigiKeyboard.print("powershell");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(3000);
+  DigiKeyboard.print("Add-Type -AssemblyName System.speech");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(100);
+  DigiKeyboard.print("$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(100);
+  //Uncomment these lines to use a female voice
+  //DigiKeyboard.print("$speak.SelectVoice('Microsoft Zira Desktop')");
+  //DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  //DigiKeyboard.delay(500);
+  DigiKeyboard.print("$speak.Speak(\"HOLA JUAKERS, UN SALUDITO BY EDUSATOE \")");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(100);
+  DigiKeyboard.print("exit");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(100);
+  DigiKeyboard.sendKeyStroke(KEY_SPACE, MOD_ALT_LEFT);
+  DigiKeyboard.sendKeyStroke(KEY_N);
+  for (;;) {
+    /*empty*/
+  }
+
+}
