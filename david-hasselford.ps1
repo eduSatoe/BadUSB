163bb73e127f7327cc06a4a2345ca075f7cae92a
Eduardo Sánchez <edu.satoe@gmail.com>
2022-12-20 13:03:39 +0100

    Delete david-hasselford.ps1

--- david-hasselford.ps1
+++
@@ -1,7 +0,0 @@
-Invoke-WebRequest -Uri "http://www.dd-inside.de/veranbilder/0.27745900%201387819383_a.jpg" -OutFile "$env:USERPROFILE\david.jpg"
-Start-Sleep -Milliseconds 500
-
-reg add "HKCU\Control Panel\Desktop" /v WallPaper /d "$env:USERPROFILE\david.jpg" /f
-Start-Sleep -Milliseconds 500
-
-Invoke-Expression -Command "rundll32.exe user32.dll, UpdatePerUserSystemParameters, 1, True"
